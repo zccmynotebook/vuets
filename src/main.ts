678391ce9b887a4b14989e492c3b8b475c5fc3c7
zcc <zcccg126@126.com>
2020-03-02 15:34:45 +0800

    add

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-//let wm=require('@zcc/watermark')
-import {waterMark} from '@zcc/watermark'
+
 Vue.config.productionTip = false
  
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
-waterMark({content: ['hello','ts','js']})
--- src/shims.lib.d.ts
+++
@@ -1,5 +0,0 @@
-
-declare module '@zcc/watermark'{
-    import * as wm from '@zcc/watermark'
-    export function waterMark(options:object):void|string
-}
